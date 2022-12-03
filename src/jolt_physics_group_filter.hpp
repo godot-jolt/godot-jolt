e5fca7f21b46edabf08c7f08dac88f8217242153
Mikael Hermansson <mikael@hermansson.io>
2022-12-03 12:33:25 +0100

    Removed source file of `JoltPhysicsGroupFilter`

--- src/jolt_physics_group_filter.hpp
+++ src/jolt_physics_group_filter.hpp
@@ -3,5 +3,8 @@
 class JoltPhysicsGroupFilter final : public JPH::GroupFilter {
 public:
 	bool CanCollide(const JPH::CollisionGroup& p_first, const JPH::CollisionGroup& p_second)
-		const override;
+		const override {
+		return (p_first.GetGroupID() & p_second.GetSubGroupID()) != 0 ||
+			(p_second.GetGroupID() & p_first.GetSubGroupID()) != 0;
+	}
 };
