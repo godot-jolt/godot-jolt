df95fe102a637bbb93f20cd22c3cb651933eecf1
Mikael Hermansson <mikael@hermansson.io>
2022-12-03 12:03:05 +0100

    Moved some stuff related to Jolt layers

---
+++ src/jolt_physics_layer_mapper_3d.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "jolt_physics_broad_phase_layer_3d.hpp"
+#include "jolt_physics_object_layer_3d.hpp"
+#include "utility_functions.hpp"
+
+class JoltPhysicsLayerMapper3D final : public JPH::BroadPhaseLayerInterface {
+public:
+	JoltPhysicsLayerMapper3D() {
+		mappings[GDJOLT_OBJECT_LAYER_STATIC] = {GDJOLT_BROAD_PHASE_LAYER_STATIC};
+		mappings[GDJOLT_OBJECT_LAYER_MOVING] = {GDJOLT_BROAD_PHASE_LAYER_MOVING};
+	}
+
+	uint32_t GetNumBroadPhaseLayers() const override { return GDJOLT_BROAD_PHASE_LAYER_COUNT; }
+
+	JPH::BroadPhaseLayer GetBroadPhaseLayer(JPH::ObjectLayer p_layer) const override {
+		return mappings[p_layer];
+	}
+
+#if defined(JPH_EXTERNAL_PROFILE) || defined(JPH_PROFILE_ENABLED)
+	const char* GetBroadPhaseLayerName(JPH::BroadPhaseLayer p_layer) const override {
+		switch ((JPH::BroadPhaseLayer::Type)p_layer) {
+			case GDJOLT_BROAD_PHASE_LAYER_STATIC: {
+				return "STATIC";
+			}
+			case GDJOLT_BROAD_PHASE_LAYER_MOVING: {
+				return "MOVING";
+			}
+			default: {
+				return "INVALID";
+			}
+		}
+	}
+#endif // JPH_EXTERNAL_PROFILE || JPH_PROFILE_ENABLED
+
+	static bool can_layers_collide(JPH::ObjectLayer p_layer1, JPH::ObjectLayer p_layer2) {
+		switch (p_layer1) {
+			case GDJOLT_OBJECT_LAYER_STATIC: {
+				return p_layer2 == GDJOLT_OBJECT_LAYER_MOVING;
+			}
+			case GDJOLT_OBJECT_LAYER_MOVING: {
+				return true;
+			}
+			default: {
+				ERR_FAIL_V_MSG(false, vformat("Unhandled object layer: '{}'", p_layer1));
+			}
+		}
+	}
+
+	static bool can_layers_collide(JPH::ObjectLayer p_layer1, JPH::BroadPhaseLayer p_layer2) {
+		switch (p_layer1) {
+			case GDJOLT_OBJECT_LAYER_STATIC: {
+				return p_layer2 == JPH::BroadPhaseLayer(GDJOLT_BROAD_PHASE_LAYER_MOVING);
+			}
+			case GDJOLT_OBJECT_LAYER_MOVING: {
+				return true;
+			}
+			default: {
+				ERR_FAIL_V_MSG(false, vformat("Unhandled object layer: '{}'", p_layer1));
+			}
+		}
+	}
+
+private:
+	JPH::BroadPhaseLayer mappings[GDJOLT_OBJECT_LAYER_COUNT] = {};
+};
