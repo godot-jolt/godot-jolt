51ee42aaa2fef9857a205552551cf58518961ee2
Mikael Hermansson <mikael@hermansson.io>
2022-12-09 10:09:09 +0100

    Made `JoltGroupFilter` a bit more verbose

--- src/jolt_group_filter.hpp
+++ src/jolt_group_filter.hpp
@@ -4,7 +4,15 @@ class JoltGroupFilter final : public JPH::GroupFilter {
 public:
 	bool CanCollide(const JPH::CollisionGroup& p_first, const JPH::CollisionGroup& p_second)
 		const override {
-		return (p_first.GetGroupID() & p_second.GetSubGroupID()) != 0 ||
-			(p_second.GetGroupID() & p_first.GetSubGroupID()) != 0;
+		const JPH::CollisionGroup::GroupID first_collision_layer = p_first.GetGroupID();
+		const JPH::CollisionGroup::SubGroupID first_collision_mask = p_second.GetSubGroupID();
+
+		const JPH::CollisionGroup::GroupID second_collision_layer = p_second.GetGroupID();
+		const JPH::CollisionGroup::SubGroupID second_collision_mask = p_second.GetSubGroupID();
+
+		const bool first_scans_second = (first_collision_mask & second_collision_layer) != 0;
+		const bool second_scans_first = (second_collision_mask & first_collision_layer) != 0;
+
+		return first_scans_second || second_scans_first;
 	}
 };
