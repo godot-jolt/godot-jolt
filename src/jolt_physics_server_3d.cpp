3fe3fb125e766c111de7c004eddac589205535b1
Mikael Hermansson <mikael@hermansson.io>
2022-12-18 01:00:34 +0100

    Added support for height map shapes (#89)

--- src/jolt_physics_server_3d.cpp
+++ src/jolt_physics_server_3d.cpp
@@ -153,7 +153,10 @@ RID JoltPhysicsServer3D::_concave_polygon_shape_create() {
 }
 
 RID JoltPhysicsServer3D::_heightmap_shape_create() {
-	ERR_FAIL_D_NOT_IMPL();
+	JoltShape3D* shape = memnew(JoltHeightMapShape3D);
+	RID rid = shape_owner.make_rid(shape);
+	shape->set_rid(rid);
+	return rid;
 }
 
 RID JoltPhysicsServer3D::_custom_shape_create() {
