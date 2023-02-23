a25c16df5d8d4e50f049de8021eaa24aac75cae2
Mikael Hermansson <mikael@hermansson.io>
2023-02-23 18:54:06 +0100

    Added type conversion for `Transform3D`

--- src/misc/type_conversions.hpp
+++ src/misc/type_conversions.hpp
@@ -8,6 +8,14 @@ _FORCE_INLINE_ Basis to_godot(const JPH::Quat& p_quat) {
 	return {Quaternion(p_quat.GetX(), p_quat.GetY(), p_quat.GetZ(), p_quat.GetW())};
 }
 
+_FORCE_INLINE_ Transform3D to_godot(const JPH::Mat44& p_mat) {
+	return {
+		Vector3(p_mat(0, 0), p_mat(1, 0), p_mat(2, 0)),
+		Vector3(p_mat(0, 1), p_mat(1, 1), p_mat(2, 1)),
+		Vector3(p_mat(0, 2), p_mat(1, 2), p_mat(2, 2)),
+		Vector3(p_mat(0, 3), p_mat(1, 3), p_mat(2, 3))};
+}
+
 _FORCE_INLINE_ Color to_godot(const JPH::Color& p_color) {
 	const auto r = (float)p_color.r;
 	const auto g = (float)p_color.g;
@@ -32,6 +40,17 @@ _FORCE_INLINE_ JPH::Vec3 to_jolt(const Vector3& p_vec) {
 _FORCE_INLINE_ JPH::Quat to_jolt(const Basis& p_basis) {
 	const Quaternion quat = p_basis.get_quaternion();
 	return {quat.x, quat.y, quat.z, quat.w};
+}
+
+_FORCE_INLINE_ JPH::Mat44 to_jolt(const Transform3D& p_transform) {
+	const Basis& b = p_transform.basis;
+	const Vector3& o = p_transform.origin;
+
+	return {
+		JPH::Vec4(b[0][0], b[1][0], b[2][0], 0.0f),
+		JPH::Vec4(b[0][1], b[1][1], b[2][1], 0.0f),
+		JPH::Vec4(b[0][2], b[1][2], b[2][2], 0.0f),
+		JPH::Vec4(o.x, o.y, o.z, 1.0f)};
 }
 
 _FORCE_INLINE_ JPH::Color to_jolt(const Color& p_color) {
