f496ecf554ac1f76c9b26ef7626eb094c84f0c19
Mikael Hermansson <mikael@hermansson.io>
2023-02-23 18:52:05 +0100

    Moved `JoltQueryFilter3D` to its own file

---
+++ src/jolt_query_filter_3d.cpp
@@ -0,0 +1,61 @@
+#include "jolt_query_filter_3d.hpp"
+
+#include "jolt_broad_phase_layer.hpp"
+#include "jolt_collision_object_3d.hpp"
+#include "jolt_physics_direct_space_state_3d.hpp"
+#include "jolt_space_3d.hpp"
+
+JoltQueryFilter3D::JoltQueryFilter3D(
+	const JoltPhysicsDirectSpaceState3D& p_space_state,
+	uint32_t p_collision_mask,
+	bool p_collide_with_bodies,
+	bool p_collide_with_areas
+)
+	: space_state(p_space_state)
+	, space(space_state.get_space())
+	, collision_mask(p_collision_mask)
+	, collide_with_bodies(p_collide_with_bodies)
+	, collide_with_areas(p_collide_with_areas) { }
+
+bool JoltQueryFilter3D::ShouldCollide(JPH::BroadPhaseLayer p_broad_phase_layer) const {
+	const auto broad_phase_layer = (JPH::BroadPhaseLayer::Type)p_broad_phase_layer;
+
+	switch (broad_phase_layer) {
+		case (JPH::BroadPhaseLayer::Type)JoltBroadPhaseLayer::BODY_STATIC:
+		case (JPH::BroadPhaseLayer::Type)JoltBroadPhaseLayer::BODY_DYNAMIC: {
+			return collide_with_bodies;
+		} break;
+		case (JPH::BroadPhaseLayer::Type)JoltBroadPhaseLayer::AREA_DETECTABLE:
+		case (JPH::BroadPhaseLayer::Type)JoltBroadPhaseLayer::AREA_UNDETECTABLE: {
+			return collide_with_areas;
+		} break;
+		default: {
+			ERR_FAIL_D_MSG(vformat("Unhandled broad phase layer: '%d'", broad_phase_layer));
+		}
+	}
+}
+
+bool JoltQueryFilter3D::ShouldCollide(JPH::ObjectLayer p_object_layer) const {
+	JPH::BroadPhaseLayer object_broad_phase_layer = {};
+	uint32_t object_collision_layer = 0;
+	uint32_t object_collision_mask = 0;
+
+	space.map_from_object_layer(
+		p_object_layer,
+		object_broad_phase_layer,
+		object_collision_layer,
+		object_collision_mask
+	);
+
+	return (collision_mask & object_collision_layer) != 0;
+}
+
+bool JoltQueryFilter3D::ShouldCollide([[maybe_unused]] const JPH::BodyID& p_body_id) const {
+	return true;
+}
+
+bool JoltQueryFilter3D::ShouldCollideLocked(const JPH::Body& p_body) const {
+	return !space_state.is_body_excluded_from_query(
+		reinterpret_cast<JoltCollisionObject3D*>(p_body.GetUserData())->get_rid()
+	);
+}
