4de44271ec6c0862642838f926103b92cfd598f7
Mikael Hermansson <mikael@hermansson.io>
2023-03-15 12:43:55 +0100

    Removed redundant collision functions for empty shape

--- src/jolt_empty_shape.cpp
+++ src/jolt_empty_shape.cpp
@@ -6,7 +6,7 @@ JPH::Shape* construct_empty() {
 	return new JoltEmptyShape();
 }
 
-void collide_empty_vs_shape(
+void collide_noop(
 	[[maybe_unused]] const JPH::Shape* p_shape1,
 	[[maybe_unused]] const JPH::Shape* p_shape2,
 	[[maybe_unused]] JPH::Vec3Arg p_scale1,
@@ -20,33 +20,7 @@ void collide_empty_vs_shape(
 	[[maybe_unused]] const JPH::ShapeFilter& p_shape_filter
 ) { }
 
-void collide_shape_vs_empty(
-	[[maybe_unused]] const JPH::Shape* p_shape1,
-	[[maybe_unused]] const JPH::Shape* p_shape2,
-	[[maybe_unused]] JPH::Vec3Arg p_scale1,
-	[[maybe_unused]] JPH::Vec3Arg p_scale2,
-	[[maybe_unused]] JPH::Mat44Arg p_center_of_mass_transform1,
-	[[maybe_unused]] JPH::Mat44Arg p_center_of_mass_transform2,
-	[[maybe_unused]] const JPH::SubShapeIDCreator& p_sub_shape_id_creator1,
-	[[maybe_unused]] const JPH::SubShapeIDCreator& p_sub_shape_id_creator2,
-	[[maybe_unused]] const JPH::CollideShapeSettings& p_collide_shape_settings,
-	[[maybe_unused]] JPH::CollideShapeCollector& p_collector,
-	[[maybe_unused]] const JPH::ShapeFilter& p_shape_filter
-) { }
-
-void cast_empty_vs_shape(
-	[[maybe_unused]] const JPH::ShapeCast& p_shape_cast,
-	[[maybe_unused]] const JPH::ShapeCastSettings& p_shape_cast_settings,
-	[[maybe_unused]] const JPH::Shape* p_shape,
-	[[maybe_unused]] JPH::Vec3Arg p_scale,
-	[[maybe_unused]] const JPH::ShapeFilter& p_shape_filter,
-	[[maybe_unused]] JPH::Mat44Arg p_center_of_mass_transform2,
-	[[maybe_unused]] const JPH::SubShapeIDCreator& p_sub_shape_id_creator1,
-	[[maybe_unused]] const JPH::SubShapeIDCreator& p_sub_shape_id_creator2,
-	[[maybe_unused]] JPH::CastShapeCollector& p_collector
-) { }
-
-void cast_shape_vs_empty(
+void cast_noop(
 	[[maybe_unused]] const JPH::ShapeCast& p_shape_cast,
 	[[maybe_unused]] const JPH::ShapeCastSettings& p_shape_cast_settings,
 	[[maybe_unused]] const JPH::Shape* p_shape,
@@ -78,25 +52,17 @@ void JoltEmptyShape::register_type() {
 		JPH::CollisionDispatch::sRegisterCollideShape(
 			JPH::EShapeSubType::User1,
 			sub_type,
-			collide_empty_vs_shape
+			collide_noop
 		);
 
 		JPH::CollisionDispatch::sRegisterCollideShape(
 			sub_type,
 			JPH::EShapeSubType::User1,
-			collide_shape_vs_empty
+			collide_noop
 		);
 
-		JPH::CollisionDispatch::sRegisterCastShape(
-			JPH::EShapeSubType::User1,
-			sub_type,
-			cast_empty_vs_shape
-		);
+		JPH::CollisionDispatch::sRegisterCastShape(JPH::EShapeSubType::User1, sub_type, cast_noop);
 
-		JPH::CollisionDispatch::sRegisterCastShape(
-			sub_type,
-			JPH::EShapeSubType::User1,
-			cast_shape_vs_empty
-		);
+		JPH::CollisionDispatch::sRegisterCastShape(sub_type, JPH::EShapeSubType::User1, cast_noop);
 	}
 }
