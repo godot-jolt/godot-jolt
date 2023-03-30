199c79c0c67c6072744634670e47dd09974e8f2d
Mikael Hermansson <mikael@hermansson.io>
2023-03-31 01:29:53 +0200

    Removed `3D` from job description

--- src/jolt_job_system.cpp
+++ src/jolt_job_system.cpp
@@ -67,7 +67,7 @@ void JoltJobSystem::Job::queue() {
 	// HACK(mihe): Ideally we would use Jolt's actual job name here, but I'd rather not incur the
 	// overhead of a memory allocation or thread-safe lookup every time we create/queue a task. So
 	// instead we use the same cached description for all of them.
-	static const String description("JoltPhysics3D");
+	static const String description("JoltPhysics");
 
 	task_id = WorkerThreadPool::get_singleton()->add_native_task(&execute, this, true, description);
 }
