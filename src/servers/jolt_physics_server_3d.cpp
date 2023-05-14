a4997c61b2e07a837fdcd2d520c2aab72860ebe8
Mikael Hermansson <mikael@hermansson.io>
2023-05-14 19:19:32 +0200

    Renamed files for `JoltHingeJointImpl3D`

--- src/servers/jolt_physics_server_3d.cpp
+++ src/servers/jolt_physics_server_3d.cpp
@@ -2,7 +2,7 @@
 
 #include "joints/jolt_cone_twist_joint_impl_3d.hpp"
 #include "joints/jolt_generic_6dof_joint_impl_3d.hpp"
-#include "joints/jolt_hinge_joint_3d.hpp"
+#include "joints/jolt_hinge_joint_impl_3d.hpp"
 #include "joints/jolt_joint_3d.hpp"
 #include "joints/jolt_pin_joint_3d.hpp"
 #include "joints/jolt_slider_joint_3d.hpp"
