e9370773ef24f7f6d7483b96c77ab7c7f23dc641
Mikael Hermansson <mikael@hermansson.io>
2023-07-12 13:13:18 +0200

    Refactored bind macros

--- src/misc/bind_macros.hpp
+++ src/misc/bind_macros.hpp
@@ -2,18 +2,21 @@
 
 #define BIND_METHOD(m_class, m_name) ClassDB::bind_method(D_METHOD(#m_name), &m_class::m_name)
 
-#define BIND_PROPERTY(m_name, m_type)  \
-	ClassDB::add_property(             \
-		get_class_static(),            \
-		PropertyInfo(m_type, #m_name), \
-		"set_" #m_name,                \
-		"get_" #m_name                 \
+#define BIND_PROPERTY(m_name, m_type) \
+	ClassDB::add_property(            \
+		get_class_static(),           \
+		PropertyInfo(m_type, m_name), \
+		"set_" m_name,                \
+		"get_" m_name                 \
 	)
 
-#define BIND_PROPERTY_ENUM(m_name, m_hint_str)                               \
-	ClassDB::add_property(                                                   \
-		get_class_static(),                                                  \
-		PropertyInfo(Variant::INT, #m_name, PROPERTY_HINT_ENUM, m_hint_str), \
-		"set_" #m_name,                                                      \
-		"get_" #m_name                                                       \
+#define BIND_PROPERTY_HINTED(m_name, m_type, m_hint, m_hint_str) \
+	ClassDB::add_property(                                       \
+		get_class_static(),                                      \
+		PropertyInfo(m_type, m_name, m_hint, m_hint_str),        \
+		"set_" m_name,                                           \
+		"get_" m_name                                            \
 	)
+
+#define BIND_PROPERTY_ENUM(m_name, m_hint_str) \
+	BIND_PROPERTY_HINTED(m_name, Variant::INT, PROPERTY_HINT_ENUM, m_hint_str)
