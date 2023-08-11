37b7edcfe271edbb826a1ce1553d7b0cc9c6458f
Mikael Hermansson <mikael@hermansson.io>
2023-08-11 16:57:42 +0200

    Changed slider joint to not flip the normal axis

--- src/joints/jolt_slider_joint_impl_3d.cpp
+++ src/joints/jolt_slider_joint_impl_3d.cpp
@@ -531,10 +531,10 @@ JPH::Constraint* JoltSliderJointImpl3D::_build_slider(
 	constraint_settings.mAutoDetectPoint = false;
 	constraint_settings.mPoint1 = to_jolt(p_shifted_ref_a.origin);
 	constraint_settings.mSliderAxis1 = to_jolt(p_shifted_ref_a.basis.get_column(Vector3::AXIS_X));
-	constraint_settings.mNormalAxis1 = to_jolt(-p_shifted_ref_a.basis.get_column(Vector3::AXIS_Z));
+	constraint_settings.mNormalAxis1 = to_jolt(p_shifted_ref_a.basis.get_column(Vector3::AXIS_Z));
 	constraint_settings.mPoint2 = to_jolt(p_shifted_ref_b.origin);
 	constraint_settings.mSliderAxis2 = to_jolt(p_shifted_ref_b.basis.get_column(Vector3::AXIS_X));
-	constraint_settings.mNormalAxis2 = to_jolt(-p_shifted_ref_b.basis.get_column(Vector3::AXIS_Z));
+	constraint_settings.mNormalAxis2 = to_jolt(p_shifted_ref_b.basis.get_column(Vector3::AXIS_Z));
 	constraint_settings.mLimitsMin = -p_limit;
 	constraint_settings.mLimitsMax = p_limit;
 
