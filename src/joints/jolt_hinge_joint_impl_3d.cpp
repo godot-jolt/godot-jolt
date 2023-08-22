8d04d5b97134b4db954cfe803fe5138197eb4d6f
Mikael Hermansson <mikael@hermansson.io>
2023-08-22 18:48:12 +0200

    Fixed hinge motors rotating the wrong way

--- src/joints/jolt_hinge_joint_impl_3d.cpp
+++ src/joints/jolt_hinge_joint_impl_3d.cpp
@@ -381,7 +381,8 @@ void JoltHingeJointImpl3D::_update_motor_velocity() {
 	QUIET_FAIL_COND(_is_fixed());
 
 	if (auto* constraint = static_cast<JPH::HingeConstraint*>(jolt_ref.GetPtr())) {
-		constraint->SetTargetAngularVelocity((float)motor_target_speed);
+		// HACK(mihe): We're forced to flip the direction of these to match Godot Physics.
+		constraint->SetTargetAngularVelocity((float)-motor_target_speed);
 	}
 }
 
