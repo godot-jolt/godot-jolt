1329120ee56bce65c90c4310b368abd37ad5c037
Mikael Hermansson <mikael@hermansson.io>
2024-02-01 19:49:10 +0100

    Updated Jolt to godot-jolt/jolt@cc49e07d7c

--- cmake/GodotJoltExternalJolt.cmake
+++ cmake/GodotJoltExternalJolt.cmake
@@ -33,7 +33,7 @@ endif()
 
 gdj_add_external_library(jolt "${configurations}"
 	GIT_REPOSITORY https://github.com/godot-jolt/jolt.git
-	GIT_COMMIT 4a74ff08e4c4f1e6cd6020b4f929ffca517d088b
+	GIT_COMMIT cc49e07d7c4eb854a60cb21ea0abdfcc93511410
 	LANGUAGE CXX
 	SOURCE_SUBDIR Build
 	OUTPUT_NAME Jolt
