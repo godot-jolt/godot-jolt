25d9067f38ebcd563e99a6ac29e9301ef4cdf1bd
Mikael Hermansson <mikael@hermansson.io>
2024-09-24 16:13:59 +0200

    Updated Jolt to godot-jolt/jolt@f15b519d62 (#986)

--- src/shapes/jolt_custom_decorated_shape.hpp
+++ src/shapes/jolt_custom_decorated_shape.hpp
@@ -155,10 +155,8 @@ public:
 	void CollideSoftBodyVertices(
 		JPH::Mat44Arg p_center_of_mass_transform,
 		JPH::Vec3Arg p_scale,
-		JPH::SoftBodyVertex* p_vertices,
+		const JPH::CollideSoftBodyVertexIterator& p_vertices,
 		JPH::uint p_num_vertices,
-		float p_delta_time,
-		JPH::Vec3Arg p_displacement_due_to_gravity,
 		int p_colliding_shape_index
 	) const override {
 		mInnerShape->CollideSoftBodyVertices(
@@ -166,8 +164,6 @@ public:
 			p_scale,
 			p_vertices,
 			p_num_vertices,
-			p_delta_time,
-			p_displacement_due_to_gravity,
 			p_colliding_shape_index
 		);
 	}
