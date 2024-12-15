65ca94f2d5fc30290145ea61e660cb16788b1748
opsocket <opsocket@pm.me>
2024-12-15 15:12:16 -0500

    🐛 enforce recomputation on mode switch

--- src/objects/jolt_body_impl_3d.hpp
+++ src/objects/jolt_body_impl_3d.hpp
@@ -233,11 +233,17 @@ public:
 
 	DampMode get_linear_damp_mode() const { return linear_damp_mode; }
 
-	void set_linear_damp_mode(DampMode p_mode) { linear_damp_mode = p_mode; }
+	void set_linear_damp_mode(DampMode p_mode) { 
+		linear_damp_mode = p_mode; 
+		_update_damp();
+	}
 
 	DampMode get_angular_damp_mode() const { return angular_damp_mode; }
 
-	void set_angular_damp_mode(DampMode p_mode) { angular_damp_mode = p_mode; }
+	void set_angular_damp_mode(DampMode p_mode) { 
+		angular_damp_mode = p_mode; 
+		_update_damp();
+	}
 
 	bool is_axis_locked(PhysicsServer3D::BodyAxis p_axis) const;
 
